c7519116c4a49cc45977f9b503d1a90e9eebbab6
kriotee <kriote@hotmail.com>
2026-08-20 23:05:13 +0200

    Update kart.js

--- script/kart.js
+++ script/kart.js
@@ -5,11 +5,11 @@ const info = document.querySelector("info");
 
 const showPlanViewTantesHus = () => {
   if (tantesHus.src.includes("tantes-hus.png")) {
-    tantesHus.src = "/image/tantes-hus-plan.png";
+    tantesHus.src = "./image/tantes-hus-plan.png";
     tantesHus.style.width = "120px";
     tantesHus.style.height = "auto";
   } else {
-    tantesHus.src = "/image/tantes-hus.png";
+    tantesHus.src = "./image/tantes-hus.png";
     tantesHus.style.width = "70px";
     tantesHus.style.height = "auto";
   }
@@ -19,10 +19,10 @@ tantesHus.addEventListener("click", showPlanViewTantesHus);
 
 const showPlanViewHytta = () => {
   if (hytta.src.includes("hytta.png")) {
-    hytta.src = "/image/hytta-plan.png";
+    hytta.src = "./image/hytta-plan.png";
     hytta.style.width = "80px";
   } else {
-    hytta.src = "/image/hytta.png";
+    hytta.src = "./image/hytta.png";
     hytta.style.width = "60px";
   }
 };
@@ -31,11 +31,11 @@ hytta.addEventListener("click", showPlanViewHytta);
 
 const showPlanViewLillehytta = () => {
   if (lillehytta.src.includes("lillehytta.png")) {
-    lillehytta.src = "/image/lillehytta-plan.png";
+    lillehytta.src = "./image/lillehytta-plan.png";
     lillehytta.style.width = "70px";
     lillehytta.style.height = "auto";
   } else {
-    lillehytta.src = "/image/lillehytta.png";
+    lillehytta.src = "./image/lillehytta.png";
     lillehytta.style.width = "40px";
     lillehytta.style.height = "auto";
   }
